fd0327ac4c624b3629bc6475516dbf1d52821a65
Lucas David Traverso <lucas6246@gmail.com>
2014-07-13 05:30:08 -0300

    First commit with app

---
+++ app/src/com/ludat/hrpg/EntryActivity.java
@@ -0,0 +1,15 @@
+package com.ludat.hrpg;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class EntryActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_entry);
+	}
+}
