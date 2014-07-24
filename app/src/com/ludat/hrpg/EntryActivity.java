6026b50aa1ddd0f9fa6c012fb46980cbf6b82103
Lucas David Traverso <lucas6246@gmail.com>
2014-07-24 09:55:01 -0300

    I achived a successful login

--- app/src/com/ludat/hrpg/EntryActivity.java
+++ app/src/com/ludat/hrpg/EntryActivity.java
@@ -1,8 +1,7 @@
 package com.ludat.hrpg;
 
 import com.ludat.hrpg.api.ApiService;
-import com.ludat.hrpg.api.ApiService.ApiBinder;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
@@ -12,45 +11,83 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.os.SystemClock;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class EntryActivity extends Activity {
 	
-	ApiService mService;
-	public boolean mBound = false;
+	private static final String TAG = "LUCAS";//EntryActivity.class.getName();
+	
+    /** Messenger for communicating with the service. */
+    Messenger mServiceMessenger = null;
+    /** Flag indicating whether we have called bind on the service. */
+    boolean mBound = false;
+    
+    @SuppressLint("HandlerLeak")
+	class EntryIncomingHandler extends Handler {
+
+    	@Override
+		public void handleMessage(Message msg){
+			switch (msg.what) {
+				case ApiService.LOGIN:
+					Log.d(TAG, "respuesta despues de logueado:" + msg.getData().getBoolean("result"));
+					break;
+				default:
+					super.handleMessage(msg);
+			}
+		}
+	};
+	final Messenger mActivityMessenger = new Messenger(new EntryIncomingHandler());
+    /**
+     * Class for interacting with the main interface of the service.
+     */
     private ServiceConnection mConnection = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(ComponentName className,
-                IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-        	new AsyncTask<String, void, String>(){
-
-				@Override
-				protected void onPostExecute(String result) {
-					// TODO Auto-generated method stub
-					super.onPostExecute(result);
-				}
-
-				@Override
-				protected String doInBackground(String... arg0) {
-					// TODO Auto-generated method stub
-					return null;
-				}
-				
-				
-        	};
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the object we can use to
+            // interact with the service.  We are communicating with the
+            // service using a Messenger, so here we get a client-side
+            // representation of that from the raw IBinder object.
+            mServiceMessenger = new Messenger(service);
+            mBound = true;
+            login();
         }
 
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mServiceMessenger = null;
             mBound = false;
         }
     };
 	
+	public void login() {
+		if (!mBound)
+			return;
+		// Create and send a message to the service, using a supported 'what'
+		// value
+		Message msg = Message.obtain(null, ApiService.LOGIN, 0, 0);
+		msg.replyTo = mActivityMessenger;
+
+		Bundle mBundle = new Bundle();
+		mBundle.putString("username", "hRPG_test");
+		mBundle.putString("password", "58963214789654123");
+		msg.setData(mBundle);
+		try {
+			mServiceMessenger.send(msg);
+		} catch (RemoteException e) {
+			Log.e(TAG, "Algo salio muy mal");
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -60,30 +97,30 @@ public class EntryActivity extends Activity {
 	@Override
 	protected void onStart() {
 		super.onStart();
-		//startService(new Intent(getApplicationContext(), ApiService.class));
 		
-		Intent bindService = new Intent(this, ApiService.class);
-		bindService(bindService, mConnection, Context.BIND_AUTO_CREATE);
+		// Bind to the service
+        bindService(new Intent(this, ApiService.class), mConnection,
+                Context.BIND_AUTO_CREATE);
 
-	};
+	}
 	
-
-    @Override
+	@Override
     protected void onStop() {
-        super.onStop();
+    	super.onStop();
         // Unbind from the service
         if (mBound) {
-            unbindService(mConnection);
-            mBound = false;
+        	unbindService(mConnection);
+        	mBound = false;
         }
     }
 	
 	public boolean isConnected(){
 		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
 		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-		if (networkInfo != null && networkInfo.isConnected()) 
+		if (networkInfo != null && networkInfo.isConnected()) {
 			return true;
-		else
+		} else {
 			return false;
+		}
 	};
 }
--- app/src/com/ludat/hrpg/api/ApiService.java
+++ app/src/com/ludat/hrpg/api/ApiService.java
@@ -1,5 +1,6 @@
 package com.ludat.hrpg.api;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,6 +12,8 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -28,91 +31,180 @@ import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
 import android.net.ParseException;
+import android.os.AsyncTask;
 import android.os.Binder;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 public class ApiService extends Service {
 
-	private static String apiKey;
-	private static String apiUser;
+	private static final String TAG = "LUCAS";//ApiService.class.getName();
 	
-	private final IBinder mBinder = new ApiBinder();
+	public static final int LOGIN = 1;
 	
-	public ApiService() {
-	}
-
-	public class ApiBinder extends Binder {
-		public ApiService getService() {
-			return ApiService.this;
+	private static String apiKey = null;
+	private static String apiUser = null;
+	
+	private Timer mTimer;
+	
+	private TimerTask updateTask = new TimerTask () {
+		@Override
+		public void run(){
+			Log.i(TAG, "I'm working");
 		}
-	}
+	};
 	
 	@Override
 	public void onCreate(){
+		super.onCreate();
+		Log.i(TAG, "Starting service");
 		apiKey = null;
 		apiUser = null;
-	}
-
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		// TODO Auto-generated method stub
-		return super.onStartCommand(intent, flags, startId);
-	}
-
-	@Override
-	public IBinder onBind(Intent intent) {
-		return mBinder;
+		mTimer = new Timer("ApiServiceTimer");
+		mTimer.schedule(updateTask, 1000L, 60 * 1000L);
 	}
 	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.i(TAG, "Destroying service");
+	     
+		mTimer.cancel();
+		mTimer = null;
+	}
+
+	@SuppressLint("HandlerLeak")
+	class IncomingHandler extends Handler {
+		@Override
+		public void handleMessage(Message msg){
+			Bundle mData = msg.getData();
+			switch (msg.what) {
+			case LOGIN:
+				Log.d(TAG, "quiero loguearme");
+				Log.d(TAG, "complete-afuera:'" + msg.toString() + "'");
+				new LoginTask(msg.replyTo, mData.getString("username"),
+						mData.getString("password")).execute();
+				break;
+			default:
+				super.handleMessage(msg);
+			}
+		}
+	}
+
+	private class LoginTask extends AsyncTask<Message, Integer, Boolean> {
+		private Messenger _replyTo;
+		private String _username;
+		private String _password;
+		
+		public LoginTask (Messenger replyTo, String username, String password) {
+			super();
+			_replyTo = replyTo;
+			_username = username;
+			_password = password;
+		}
+		
+		protected Boolean doInBackground(Message... messages) {
+
+			Log.d(TAG, "username:'" + _username + "'");
+			Log.d(TAG, "password:'" + _password + "'");
+			Message replyMsg = Message.obtain(null, ApiService.LOGIN, 0, 0);
+			Bundle responseData = new Bundle();
+			
+			Boolean result = ApiService.this.login(_username, _password);
+			
+			responseData.putBoolean("result", result);
+			replyMsg.setData(responseData);
+			try {
+				_replyTo.send(replyMsg);
+			} catch (RemoteException e) {
+				Log.e(TAG, "Algo salio muy mal");
+				e.printStackTrace();
+			}
+			return result;
+		}
+
+		protected void onProgressUpdate(Integer... progress) {
+		}
+
+		protected void onPostExecute(Boolean result) {
+		}
+	}
+	
+	final Messenger mMessenger = new Messenger(new IncomingHandler());
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		return mMessenger.getBinder();
+	}
+	
+	// My functions
 	public boolean login(String username, String password){
 		InputStream is = null;
 		final String myurl = "https://habitrpg.com/api/v2/user/auth/local";
+		
+		URL url = null;
+		HttpsURLConnection conn = null;
 		try {
-			URL url = new URL(myurl);
-	    	HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-	        conn.setReadTimeout(10000 /* milliseconds */);
-	        conn.setConnectTimeout(15000 /* milliseconds */);
+			url = new URL(myurl);
+			conn = (HttpsURLConnection) url.openConnection();
+	        conn.setReadTimeout(100000 /* milliseconds */);
+	        conn.setConnectTimeout(150000 /* milliseconds */);
 	        conn.setRequestMethod("POST");
 	        conn.setDoInput(true);
-	        conn.setDoOutput(true);
 
-	        conn.addRequestProperty("Accept", "application/json");
 			conn.addRequestProperty("Content-type", "application/json");
 
 			JSONObject jsonObject = new JSONObject();
 			jsonObject.put("username", username);
 			jsonObject.put("password", password);
+			Log.e(TAG, "JSON:" + jsonObject.toString());
 
-			Log.e("LUCAS", "1");
-			OutputStream os = conn.getOutputStream();
-			Log.e("LUCAS", "3");
-			os.write(jsonObject.toString().getBytes("UTF-8"));
+			Log.e(TAG, "1");
+			OutputStreamWriter os = new OutputStreamWriter(conn.getOutputStream());
+			Log.e(TAG, "2");
+			os.write(jsonObject.toString());
+			Log.e(TAG, "3");
+
 			os.close();
-
-			int response = conn.getResponseCode();
-			Log.e("LUCAS", "The response is: " + response);
+			Log.e(TAG, "4");
+			
+			conn.connect();
+			
+			// This is really weird
 			is = conn.getInputStream();
-	
+			Log.e(TAG, "5");
 	        // Convert the InputStream into a string
 	        String contentAsString = readIt(is);
+			Log.e(TAG, "Content: " + contentAsString);
 	        
 			// Makes sure that the InputStream is closed after the app is
 			// finished using it.
+			
 			JSONObject responseJson = new JSONObject(contentAsString);
-			if (! responseJson.has("err") && responseJson.has("username") && responseJson.has("password")){
-				apiUser = responseJson.getString("username");
-				apiKey = responseJson.getString("password");
-				Log.e("LUCAS", "HOLY SHIT YES");
+			if (! responseJson.has("err") && responseJson.has("id") && responseJson.has("token")){
+				apiUser = responseJson.getString("id");
+				apiKey = responseJson.getString("token");
+				Log.e(TAG, "HOLY SHIT YES");
 				return true;
 			}
 			return false;
 	    } catch (Exception e) {
-	    	Log.e("LUCAS", "ALGO PASO CON LA CONEXION:\"" + e.toString() + "\"");
-	    	Log.e("LUCAS", "ALGO PASO CON LA CONEXION:\"" + e.getLocalizedMessage() + "\"");
+	    	Log.e(TAG, "ALGO PASO CON LA CONEXION:\"" + e.toString() + "\"");
+	    	try {
+				Log.e(TAG, "CODIGO DE LA CONEXION:\"" + conn.getResponseCode() + "\"");
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			e.printStackTrace();
 			return false;
 		} finally {
@@ -122,20 +214,20 @@ public class ApiService extends Service {
 	    		try {
 					is.close();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 	    	}
 		}
 	}
 	    
-	    // Reads an InputStream and converts it to a String.
-	    public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
-	    	int len = 500;
-	        Reader reader = null;
-	        reader = new InputStreamReader(stream, "UTF-8");    
-	        char[] buffer = new char[len];
-	        reader.read(buffer);
-	        return new String(buffer);
-	    }
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream) throws IOException {
+    	BufferedReader r = new BufferedReader(new InputStreamReader(stream));
+    	StringBuilder total = new StringBuilder();
+    	String line;
+    	while ((line = r.readLine()) != null) {
+    	    total.append(line);
+    	}
+    	return total.toString();
+    }
 }
